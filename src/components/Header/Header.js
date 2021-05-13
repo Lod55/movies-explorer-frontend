e940da64b53b0840b6bccf0ed192633624ebe622
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-05-13 10:09:21 +0500

    feat: Header js/css component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.png';
+import AuthNav from "../AuthNav/AuthNav";
+
+const Header = () => {
+  return (
+    <header className={'header'}>
+      <Link to="/">
+        <img src={logo} alt="Логотип проекта" className="header__logo"/>
+      </Link>
+      <AuthNav/>
+    </header>
+  );
+};
+
+export default Header;
