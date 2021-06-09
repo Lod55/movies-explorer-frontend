68558f1b938f3c571e14b76ef9585027f1eb106f
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-09 15:35:30 +0500

    feat: add Movies functionality

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,30 +1,79 @@
-import "./MoviesCard.css";
-import React, { useState } from "react";
-import defaultPhoto from "../../images/photo-movie.jpg";
+import './MoviesCard.css';
+import React, { useEffect, useState } from 'react';
+import defaultPhoto from '../../images/photo-movie.jpg';
 
 function MoviesCard(props) {
-  const { listType, isMovieSave } = props;
-  const [isSaved, getIsSaved] = useState(isMovieSave);
+  const { listType, movie, saveMovie, removeMovie, savedMoviesIds } = props;
 
-  const handleClickSave = () => {
-    getIsSaved(!isSaved);
+  const {
+    country,
+    director,
+    year,
+    description,
+    image,
+    thumbnail,
+    nameRU,
+    nameEN,
+    duration,
+    trailer,
+    movieId,
+  } = movie;
+
+  const [isSaved, getIsSaved] = useState(false);
+
+  const hours = Math.floor(duration / 60);
+  const minutes = duration - hours * 60;
+  const durationStr = hours ? `${hours}ч ${minutes}м` : `${minutes}мин`;
+
+  const handleSave = () => {
+    saveMovie({
+      country: country || 'Данные отсутствуют',
+      director: director || 'Данные отсутствуют',
+      duration: duration || 0,
+      year: year || 'Данные отсутствуют',
+      description: description || '',
+      image: image || 'https://unsplash.com/photos/49uySSA678U',
+      trailer: trailer || 'https://youtube.com',
+      thumbnail: thumbnail || 'https://unsplash.com/photos/49uySSA678U',
+      nameRU: nameRU || 'Данные отсутствуют',
+      nameEN: nameEN || 'Данные отсутствуют',
+      movieId,
+    });
   };
+
+  const handleRemove = () => {
+    removeMovie({ movieId });
+  };
+
+  const handleClickImg = () => {
+    window.open(trailer);
+  };
+
+  useEffect(() => {
+    return savedMoviesIds && savedMoviesIds.includes(movieId)
+      ? getIsSaved(true)
+      : getIsSaved(false);
+  }, [savedMoviesIds, movieId]);
+
   return (
     <article className="movie">
-      <h2 className="movie__title">33 слова о дизайне</h2>
-      <p className="movie__time">1ч 47м</p>
-      {listType === "main" ? (
+      <h2 className="movie__title">{nameRU}</h2>
+      <p className="movie__time">{durationStr}</p>
+      {listType === 'main' ? (
         <button
-          className={`movie__button-save ${
-            isSaved ? "movie__button-save_active" : ""
-          }`}
+          className={`movie__button-save ${isSaved ? 'movie__button-save_active' : ''}`}
           type="button"
-          onClick={handleClickSave}
+          onClick={!isSaved ? handleSave : handleRemove}
         />
       ) : (
-        <button className={"movie__button-delete"} type="button" />
+        <button className={'movie__button-delete'} type="button" onClick={handleRemove} />
       )}
-      <img className="movie__img" alt="Фотография фильма" src={defaultPhoto} />
+      <img
+        className="movie__img"
+        alt={nameRU}
+        src={image || defaultPhoto}
+        onClick={handleClickImg}
+      />
     </article>
   );
 }
--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,18 +1,25 @@
-import React from "react";
-import "./SavedMovies.css";
-import Header from "../Header/Header";
-import Content from "../Content/Content";
-import SearchForm from "../SearchForm/SearchForm";
-import MoviesCardList from "../MoviesCardList/MoviesCardList";
-import Footer from "../Footer/Footer";
+import React from 'react';
+import './SavedMovies.css';
 
-const SavedMovies = () => {
+import Header from '../Header/Header';
+import Content from '../Content/Content';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
+
+const SavedMovies = (props) => {
+  const { isLoggedIn, isLoading, movies, removeMovie, searchMovies } = props;
   return (
     <>
-      <Header />
+      <Header isLoggedIn={isLoggedIn} />
       <Content>
-        <SearchForm />
-        <MoviesCardList listType="save" />
+        <SearchForm searchMovies={searchMovies} />
+        <MoviesCardList
+          listType="save"
+          isLoading={isLoading}
+          movies={movies}
+          removeMovie={removeMovie}
+        />
       </Content>
       <Footer />
     </>
