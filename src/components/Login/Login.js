7723e5e27d90c08bb54bac6dc2967ec110a78372
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-09 15:29:33 +0500

    feat: add Login functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,29 @@
-import "./Login.css";
-import React from "react";
-import Content from "../Content/Content";
-import AuthForm from "../AuthForm/AuthForm";
-import Error from "../Error/Error";
+import './Login.css';
+import React from 'react';
 
-const Login = () => {
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
+
+import { patterns } from '../../utils/constants';
+
+import Content from '../Content/Content';
+import AuthForm from '../AuthForm/AuthForm';
+import Error from '../Error/Error';
+
+const Login = (props) => {
+  const { onLogin } = props;
+
+  const { values, handleChange, resetFrom, errors, isValid, isValidInputs } =
+    useFormWithValidation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onLogin({
+      email: values.emailLog,
+      password: values.passwordLog,
+    }).then(resetFrom);
+  };
+
   return (
     <Content>
       <AuthForm
@@ -14,37 +33,44 @@ const Login = () => {
         text="Ещё не зарегистрированы?"
         pathLink="/sign-up"
         textLink="Регистрация"
+        onSubmit={handleSubmit}
+        isValid={isValid}
       >
-        <label className="auth-form__label" htmlFor="email-login">
+        <label className="auth-form__label" htmlFor="emailLog">
           E-mail
         </label>
         <input
-          className="auth-form__input"
+          className={`auth-form__input ${
+            isValidInputs.emailLog ? 'auth-form__input_state_valid' : ''
+          }`}
           type="email"
-          name="email-login"
-          id="email-login"
+          name="emailLog"
+          id="emailLog"
+          value={values.emailLog || ''}
+          onChange={handleChange}
+          pattern={patterns.email}
           required
         />
-        <Error
-          className="auth-form__error"
-          id="login-input-email-error"
-          text=""
-        />
+        <Error className="auth-form__error" id="login-input-email-error" text={errors.emailLog} />
 
-        <label className="auth-form__label" htmlFor="password-login">
+        <label className="auth-form__label" htmlFor="passwordLog">
           Пароль
         </label>
         <input
-          className="auth-form__input"
+          className={`auth-form__input ${
+            isValidInputs.passwordLog ? 'auth-form__input_state_valid' : ''
+          }`}
           type="password"
-          name="password-login"
-          id="password-login"
+          name="passwordLog"
+          id="passwordLog"
+          value={values.passwordLog || ''}
+          onChange={handleChange}
           required
         />
         <Error
           className="auth-form__error"
           id="password-input-email-error"
-          text=""
+          text={errors.passwordLog}
         />
       </AuthForm>
     </Content>
