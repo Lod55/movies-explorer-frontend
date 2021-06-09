c8a86fe8f7a09a76ee644a2e3cc54270df965d72
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-06-09 15:28:04 +0500

    update: Error

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,8 +1,8 @@
-import "./Error.css";
-import React from "react";
+import './Error.css';
+import React from 'react';
 
 const Error = (props) => {
-  const { className, text, id } = props;
+  const { className = '', text = '', id = '' } = props;
   return (
     <span className={`error ${className}`} id={id}>
       {text}
